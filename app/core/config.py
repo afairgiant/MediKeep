5e805f15cc27d31778e723fc6c5a271eb2671661
Alex Fair <20632147+afairgiant@users.noreply.github.com>
2025-06-12 23:48:49 -0400

    bump to version 0.1.0

--- app/core/config.py
+++ app/core/config.py
@@ -14,7 +14,7 @@ DB_NAME = os.getenv("DB_NAME", "")
 
 class Settings:  # App Info
     APP_NAME: str = "Medical Records Management System"
-    VERSION: str = "0.0.11"
+    VERSION: str = "0.1.0"
     DEBUG: bool = (
         os.getenv("DEBUG", "True").lower() == "true"
     )  # Enable debug by default in development    # Database Configuration
