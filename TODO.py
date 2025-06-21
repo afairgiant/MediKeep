2ba03d00a122aeb552dfcf3a9118442ee76f8842
Alex Fair <20632147+afairgiant@users.noreply.github.com>
2025-06-21 10:09:32 -0400

    feat: added pharmacy model

--- TODO.py
+++ TODO.py
@@ -1,2 +1,5 @@
 # TODO
 #
+# 1. Make sure that when a doctor isn't deleted the other items that have an associated doctor are also not deleted. it should be set to null instead.
+# 2. Add pharmacy to medication model.
+# 3. Add doctor to medication page. Already setup in database model.
