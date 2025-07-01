e5fa9e186cd8b673752dfe0b5d4326b523cf318e
Alex Fair <20632147+afairgiant@users.noreply.github.com>
2025-07-01 13:38:08 -0400

    feat: update TODO list with bug fixes and improvements

--- TODO.py
+++ TODO.py
@@ -1,7 +1,37 @@
 # TODO
-#
-# 1. Make sure that when a doctor isn't deleted the other items that have an associated doctor are also not deleted. it should be set to null instead.
-# 2. Add pharmacy to medication model.
-# 3. Add doctor to medication page. Already setup in database model.
-# 4. Make the cards on the main dashboard can have their size adjusted.
-# 5. Make a settings page.
+"""
+List of bugs:
+    treatments page: 
+        Doseage is not being displayed correctly.
+        Doseage is not updating correctly.
+        Treatment description is not being displayed correctly.
+
+
+    Procedure Page:
+        Make procedure type more visable on card.
+
+    Vitals Page:
+        Make the bp N/A not bold.
+        Make the BMI N/A not bold.
+        The filter sorting is broken.
+        When adding/editing a vital, change from date to datetime picker.
+        Show additional information on the vital card or have a view button to see the description.
+        
+Improvments:
+    Conditions:
+        Add end date to condition.
+    Visits:
+        Could use more information?
+
+    Patient Page:
+        Don't need to show the patient ID on the patient page.
+        Make the weight in patient page linked to the vitals page for the latest weight.
+
+
+    Nav Bar:
+        Add links to all the pages in the nav bar.
+        Shift the title over to the left to make room for links?
+
+    Admin:
+        User: Make more user info show up on the user page.
+"""
