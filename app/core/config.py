496d24f4cc471a27c809445bbb776ee6ba9880c5
Alex Fair <20632147+afairgiant@users.noreply.github.com>
2025-07-21 19:51:03 -0400

    chore: update version to 0.16.0 in config.py

--- app/core/config.py
+++ app/core/config.py
@@ -15,7 +15,7 @@ DB_NAME = os.getenv("DB_NAME", "")
 
 class Settings:  # App Info
     APP_NAME: str = "Medical Records Management System"
-    VERSION: str = "0.15.1"
+    VERSION: str = "0.16.0"
 
     DEBUG: bool = (
         os.getenv("DEBUG", "True").lower() == "true"
