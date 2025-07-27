be75c47373d3bddda2c71a16329618ab3a9873d8
Alex Fair <20632147+afairgiant@users.noreply.github.com>
2025-07-27 12:16:37 -0400

    Update version to 0.19.0 in Settings class

--- app/core/config.py
+++ app/core/config.py
@@ -16,7 +16,7 @@ DB_NAME = os.getenv("DB_NAME", "")
 
 class Settings:  # App Info
     APP_NAME: str = "Medical Records Management System"
-    VERSION: str = "0.18.1"
+    VERSION: str = "0.19.0"
 
     DEBUG: bool = (
         os.getenv("DEBUG", "True").lower() == "true"
