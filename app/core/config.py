6529dc01d7ca8f65e4c44e6668b2bb92721164ca
Alex Fair <20632147+afairgiant@users.noreply.github.com>
2025-09-16 17:56:14 -0400

    chore: bump version to 0.27.8

--- app/core/config.py
+++ app/core/config.py
@@ -17,7 +17,7 @@ DB_NAME = os.getenv("DB_NAME", "")
 
 class Settings:  # App Info
     APP_NAME: str = "Medical Records Management System"
-    VERSION: str = "0.27.7"
+    VERSION: str = "0.27.8"
 
     DEBUG: bool = (
         os.getenv("DEBUG", "True").lower() == "true"
