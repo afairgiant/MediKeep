a34678d08125044896767aaa8b55e05bb5f72604
Alex Fair <20632147+afairgiant@users.noreply.github.com>
2025-10-23 19:51:21 -0400

    chore: update application version to 0.38.0 in config.py

--- app/core/config.py
+++ app/core/config.py
@@ -48,7 +48,7 @@ DB_NAME = os.getenv("DB_NAME", "")
 
 class Settings:  # App Info
     APP_NAME: str = "MediKeep"
-    VERSION: str = "0.37.0"
+    VERSION: str = "0.38.0"
 
     DEBUG: bool = (
         os.getenv("DEBUG", "True").lower() == "true"
