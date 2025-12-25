bd8b3ade2a7a71bedc24758146aed43c41aae060
Alex Fair <20632147+afairgiant@users.noreply.github.com>
2025-12-25 10:41:20 -0500

    chore: increment application version to 0.44.0 in config.py

--- app/core/config.py
+++ app/core/config.py
@@ -55,7 +55,7 @@ DB_PASS = quote_plus(_DB_PASS_RAW) if _DB_PASS_RAW else ""
 
 class Settings:  # App Info
     APP_NAME: str = "MediKeep"
-    VERSION: str = "0.43.0"
+    VERSION: str = "0.44.0"
 
     DEBUG: bool = (
         os.getenv("DEBUG", "True").lower() == "true"
