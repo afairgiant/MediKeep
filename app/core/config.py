b9b0a0f150d406dab41cfe8a0a2882ee7df49a32
afairgiant <alex.j.fair@gmail.com>
2026-07-01 22:31:32 -0400

    chore: bump version to 0.68.0

--- app/core/config.py
+++ app/core/config.py
@@ -108,7 +108,7 @@ def _derive_salt(purpose: str) -> str:
 
 class Settings:  # App Info
     APP_NAME: str = "MediKeep"
-    VERSION: str = "0.67.0"
+    VERSION: str = "0.68.0"
 
     DEBUG: bool = os.getenv("DEBUG", "False").lower() == "true"
 
